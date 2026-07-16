dfd9bf7a9382e0a67dcf6f5c78e56de7e6de1cff
Owoeye Elizabeth <owoeyeelizabeth22@gmail.com>
2026-07-16 14:45:41 +0100

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,16 @@ const mysql = require("mysql2");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
 const app = express();
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Il0vecooking",
-    database: "adefam"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
