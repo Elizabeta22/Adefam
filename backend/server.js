0edcd732203c05ceadaf2547dd13685152b04f89
Owoeye Elizabeth <owoeyeelizabeth22@gmail.com>
2026-08-24 12:12:02 +0100

    server edit

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,12 @@ require("dotenv").config();
 const app = express();
 
 // Middleware
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: 'https://adefam-cf4t-xi.vercel.app/',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // =========================
 // MySQL Connection Pool
